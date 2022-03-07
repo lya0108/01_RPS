2828b925a286852b3b3a607617054f22c69c6a3f
lya0108 <lya0108@masseyhigh.school.nz>
2022-03-07 15:42:22 +1300

    Create 02_user_choice_v1.py

---
+++ 02_user_choice_v1.py
@@ -0,0 +1,37 @@
+def choice_checker(question):
+
+    error = "Please choose from rock / paper / scissors (or xxx to quit)"
+
+    valid = False
+    while not valid:
+
+        rock = ["rock", "r"]
+        paper = ["paper", "p"]
+        scissors = ["scissors", "s"]
+
+        # asks user for choice
+        response = input(question).lower()
+
+        if response in rock:
+            response = "rock"
+            return response
+
+        elif response in paper:
+            response = "paper"
+            return response
+
+        elif response in scissors:
+            response = "scissors"
+            return response
+
+        else:
+            print(error)
+
+# main routine
+
+
+# loop for testing
+user_choice = ""
+while user_choice != "xxx":
+    
+    # asks
